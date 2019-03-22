aab163af4921b2a02e424550b11fdcb60ca7e840
Anne Poso <anneposo1@csu.fullerton.edu>
2019-03-21 20:08:21 -0700

    Removed line address function

--- ed.c
+++ ed.c
@@ -122,82 +122,10 @@ void print(void) {
 	pflag = 0;
 }
 
-unsigned int *
-address(void) {
-	int sign;
-	unsigned int *a, *b;
-	int opcnt, nextopand;
-	int c;
 
-	nextopand = -1;
-	sign = 1;
-	opcnt = 0;
-	a = dot;
-	do {
-		do c = getchr(); while (c==' ' || c=='\t');
-		if ('0'<=c && c<='9') {
-			peekc = c;
-			if (!opcnt)
-				a = zero;
-			a += sign*getnum();
-		} else switch (c) {
-		case '$':
-			a = dol;
-			/* fall through */
-		case '.':
-			if (opcnt)
-				error(Q);
-			break;
-		case '\'':
-			c = getchr();
-			if (opcnt || c<'a' || 'z'<c)
-				error(Q);
-			a = zero;
-			do a++; while (a<=dol && names[c-'a']!=(*a&~01));
-			break;
-		case '?':
-			sign = -sign;
-			/* fall through */
-		case '/':
-			compile(c);
-			b = a;
-			for (;;) {
-				a += sign;
-				if (a<=zero)
-					a = dol;
-				if (a>dol)
-					a = zero;
-				if (execute(a))
-					break;
-				if (a==b)
-					error(Q);
-			}
-			break;
-		default:
-			if (nextopand == opcnt) {
-				a += sign;
-				if (a<zero || dol<a)
-					continue;       /* error(Q); */
-			}
-			if (c!='+' && c!='-' && c!='^') {
-				peekc = c;
-				if (opcnt==0)
-					a = 0;
-				return (a);
-			}
-			sign = 1;
-			if (c!='+')
-				sign = -sign;
-			nextopand = ++opcnt;
-			continue;
-		}
-		sign = 1;
-		opcnt++;
-	} while (zero<=a && a<=dol);
-	error(Q);
-	/*NOTREACHED*/
-	return 0;
-}
+//****************************************address represents the number of a line in the buffer - typically supplied to commands as default
+//****************************************REMOVED address() - line address not used for grep
+
 
 int getnum(void) {
 	int r, c;
