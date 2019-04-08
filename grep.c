769be97646701e4e798d5b5a873dc346f82763bd
Anne Poso <anneposo1@csu.fullerton.edu>
2019-04-07 23:58:37 -0700

    Removed commands() function

--- grep.c
+++ grep.c
@@ -26,37 +26,6 @@ void regexp_buf_init(const char* regexp){
    //rbufp = regexp_buf;
 }
 
-void commands(void) {  unsigned int *a1;  int c, temp;  char lastsep;
-  for (;;) {
-    if (pflag) { pflag = 0;  addr1 = addr2 = dot;  print(); }  c = '\n';
-    for (addr1 = 0;;) {
-      lastsep = c;  a1 = address();  c = getchr();
-      if (c != ',' && c != ';') { break; }  if (lastsep==',') { error(Q); }
-      if (a1==0) {  a1 = zero+1;  if (a1 > dol) { a1--; }  }  addr1 = a1;  if (c == ';') { dot = a1; }
-    }
-    if (lastsep != '\n' && a1 == 0) { a1 = dol; }
-    if ((addr2 = a1)==0) { given = 0;  addr2 = dot;  } else { given = 1; }
-    if (addr1==0) { addr1 = addr2; }
-    switch(c) {
-    case EOF:  return;
-    case '\n':  if (a1 == 0) { a1 = dot + 1;  addr2 = a1;  addr1 = a1; }
-                if (lastsep == ';') { addr1 = a1; }  print();  continue;
-    case 'g':  global(1);  continue;  //grep command
-    case 'p':  case 'P':  print_genbuf();  continue; // prints all/select lines from text file
-    case 'Q':  fchange = 0;  case 'q':  setnoaddr();  newline();  quit(0); // quits program
-    //case 'e':  setnoaddr(); if (vflag && fchange) { fchange = 0;  error(Q); } filename(c);  init(); //************ 'e' deals with loading the text file to search regexp strings
-          //     addr2 = zero;  caseread(c); continue;
-    case 'z':  grepline();  continue;
-    caseGrepError: default:  greperror(c);  continue;
-    }  error(Q);
-  }
-}
-//created function caseread to remove goto statements
-void caseread(const char* c) {
-  if ((io = open((const char*)file, 0)) < 0) { lastc = '\n';  error(file); }  setwide();  squeeze(0);
-         ninbuf = 0; // c = zero != dol;
-  append(getfile, addr2); exfile(); /* fchange = c; */}
-
 void readfile(const char* textfile){
   setnoaddr(); if (vflag && fchange) { fchange = 0;  error(Q); }
   filename(textfile);  init();
