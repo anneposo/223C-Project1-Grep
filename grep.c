919477addcabe5bbb88d50b0429a8d16cd8f17bd
Anne Poso <anneposo1@csu.fullerton.edu>
2019-04-10 17:15:56 -0700

    Removed address() function

--- grep.c
+++ grep.c
@@ -204,37 +204,6 @@ void reverse_(char* s){ // reverses input string for print_genbuf() function
   }
 }
 
-unsigned int* address(void) {
-  int sign = 1, opcnt = 0, nextopand = -1, c;
-  unsigned int *a, *b;
-  a = dot;
-  do {
-    do c = getchr(); while (c==' ' || c=='\t');
-    if ('0'<=c && c<='9') {
-      peekc = c;
-      if (!opcnt)  { a = zero; }
-      a += sign*getnum();
-    } else switch (c) {
-      case '$':  a = dol;  /* fall through */
-      case '.':  if (opcnt) { error(Q); } break;
-      case '\'': c = getchr();
-        if (opcnt || c<'a' || 'z'<c) { error(Q); }
-        a = zero;
-        do { a++; } while (a<=dol && names[c-'a']!=(*a&~01));
-        break;
-      case '?':  sign = -sign;  /* fall through */
-      case '/': compile(c);  b = a;
-        for (;;) { a += sign; if (a<=zero) { a = dol; }  if (a>dol) { a = zero; }  if (execute(a)) { break; }  if (a==b)  { error(Q); } }
-        break;
-      default: if (nextopand == opcnt) {  a += sign;  if (a < zero || dol < a)  { continue; } /* error(Q); */ }
-        if (c!='+' && c!='-' && c!='^') {  peekc = c;  if (opcnt==0) { a = 0; }  return (a);  }
-        sign = 1;  if (c!='+') { sign = -sign; }  nextopand = ++opcnt;  continue;
-    }
-    sign = 1;
-    opcnt++;
-  } while (zero<=a && a<=dol);
-  error(Q);  /*NOTREACHED*/  return 0;
-}
 int advance(char *lp, char *ep) {  char *curlp;  int i;
   for (;;) {
     switch (*ep++) {
@@ -336,7 +305,6 @@ int execute(unsigned int *addr) {  char *p1, *p2 = expbuf;
   } while (*p1++);
   return(0);
 }
-
 
 char * getblock(unsigned int atl, int iof) {  int off = (atl<<1) & (BLKSIZE-1) & ~03, bno = (atl/(BLKSIZE/2));
   if (bno >= NBLK) {  lastc = '\n';  error(T);  }
